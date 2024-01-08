ad0380c9001f52d56ddcff7dcb44e028c54f9f56
Keshanth Jude <keshanth64@gmail.com>
2024-01-08 12:55:39 +0000

    Updated user login bug

--- App.js
+++ App.js
@@ -1,28 +1,33 @@
-import { SafeAreaView, Text, AppState, Alert, Linking, RefreshControl, ScrollView } from "react-native";
-import { useEffect, useState, useRef, useCallback } from 'react';
+import { SafeAreaView, Text, AppState, Alert, Linking, RefreshControl, ScrollView } from "react-native"
+import { useEffect, useState, useRef, useCallback } from 'react'
 import { WebView } from 'react-native-webview'
-import OneSignal from 'react-native-onesignal';
-import Constants from 'expo-constants';
-import 'expo-dev-client';
-import Geolocation from '@react-native-community/geolocation';
+import OneSignal from 'react-native-onesignal'
+import Constants from 'expo-constants'
+import 'expo-dev-client'
+import Geolocation from '@react-native-community/geolocation'
 // https://stackoverflow.com/questions/54075629/reactnative-permission-always-return-never-ask-again
 
-import { maybeSetUserLocation, getExternalUIDInWP, GetAllPermissions } from './utils'
+const INJECTED_JAVASCRIPT = `(function() {
+    const allData = window.localStorage.getItem('ccevents_ukey');
+    window.ReactNativeWebView.postMessage(allData);
+})();`
+
+import { maybeSetUserLocation, getExternalUIDInWP, GetAllPermissions, URL, loginUserinWP } from './utils'
 
 export default function App() {
-  const [carcalSession, setcarcalSession] = useState('');
-  const [onesignalRegistered, setOnesignalRegistered] = useState(false);
-  const [playerId, setPlayerId] = useState('');
-  const [location, setLocation] = useState();
-  const [permissionsLocation, setPermissionsLocation] = useState({ denied: false, granted: false });
+  const [carcalSession, setcarcalSession] = useState('')
+  const [onesignalRegistered, setOnesignalRegistered] = useState(false)
+  const [playerId, setPlayerId] = useState('')
+  const [location, setLocation] = useState()
+  const [permissionsLocation, setPermissionsLocation] = useState({ denied: false, granted: false })
 
-  const appState = useRef(AppState.currentState);
-  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+  const appState = useRef(AppState.currentState)
+  const [appStateVisible, setAppStateVisible] = useState(appState.current)
 
-  const [refreshing, setRefreshing] = useState(false);
+  const [refreshing, setRefreshing] = useState(false)
   const webViewRef = useRef()
 
-  const [refresherEnabled, setEnableRefresher] = useState(true);
+  const [refresherEnabled, setEnableRefresher] = useState(true)
 
   //Code to get scroll position
   const handleScroll = (event) => {
@@ -32,90 +37,89 @@ export default function App() {
   }
 
   const onRefresh = useCallback(() => {
-    setRefreshing(true);
-    webViewRef.current.reload();
+    setRefreshing(true)
+    webViewRef.current.reload()
     setTimeout(() => {
-      setRefreshing(false);
-    }, 1000);
-  }, []);
+      setRefreshing(false)
+    }, 1000)
+  }, [])
 
 
   // Get the users current location
   const getCurrentPosition = () => {
     Geolocation.getCurrentPosition((pos) => {
-      if (pos.coords) setLocation(pos.coords);
+      if (pos.coords) setLocation(pos.coords)
     }, (error) => {
       Alert.alert("CarCalendar", error.message, [
         { text: "OK" },
         {
           text: "Settings",
           onPress: () => {
-            Linking.openSettings();
+            Linking.openSettings()
           }
         }
 
       ])
     },
       { enableHighAccuracy: true }
-    );
-  };
+    )
+  }
 
   // OneSignal Initialization
   useEffect(() => {
-    OneSignal.setAppId(Constants.manifest.extra.onesignal.app_id);
+    OneSignal.setAppId(Constants.manifest.extra.onesignal.app_id)
     OneSignal.addSubscriptionObserver((event) => {
       // if event.to is true, the user is subscribed
       // console.log(`OneSignal Subscription Changed: ${event}`);
       if (event.to) {
         // get user id 
         OneSignal.getDeviceState().then((deviceState) => {
-          console.log(`OneSignal Player ID: ${deviceState.userId}`);
-          setPlayerId(deviceState.userId);
-        });
+          console.log(`OneSignal Player ID: ${deviceState.userId}`)
+          setPlayerId(deviceState.userId)
+        })
       }
     });
 
     // Get the user permissions for location and notifications
     (async () => {
-      let res = await GetAllPermissions();
+      let res = await GetAllPermissions()
 
       // Location
       if (res["android.permission.ACCESS_FINE_LOCATION"] === "granted") {
         setPermissionsLocation({ denied: false, granted: true })
-        getCurrentPosition();
-      } else setPermissionsLocation({ denied: true, granted: false });
+        getCurrentPosition()
+      } else setPermissionsLocation({ denied: true, granted: false })
 
       // Notifications
       if (res["android.permission.POST_NOTIFICATIONS"] === "granted") {
-        console.log('Notification permission granted');
+        console.log('Notification permission granted')
       } else {
-        console.log('Notification permission denied');
+        console.log('Notification permission denied')
       }
-    })();
+    })()
 
     OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent) => {
-      let notification = notificationReceivedEvent.getNotification();
-      let notifId = notification.notificationId;
-      let title = notification.title;
+      let notification = notificationReceivedEvent.getNotification()
+      let notifId = notification.notificationId
+      let title = notification.title
 
-      console.log(`OneSignal New Notif> ID: ${notifId}, Title: ${title}`);
+      console.log(`OneSignal New Notif> ID: ${notifId}, Title: ${title}`)
 
-      notificationReceivedEvent.complete(notification);
-    });
+      notificationReceivedEvent.complete(notification)
+    })
 
     OneSignal.setNotificationOpenedHandler((notification) => {
-      console.log('OneSignal: notification opened:', notification);
-    });
+      console.log('OneSignal: notification opened:', notification)
+    })
 
     OneSignal.setInAppMessageClickHandler((event) => {
-      console.log('OneSignal IAM clicked:', event);
-    });
+      console.log('OneSignal IAM clicked:', event)
+    })
 
     return () => {
       OneSignal.clearHandlers()
     }
-  }, []);
-
+  }, [])
 
   // Handles the AppState
   useEffect(() => {
@@ -124,52 +128,57 @@ export default function App() {
         appState.current.match(/inactive|background/) &&
         nextAppState === 'active'
       ) {
-        console.log('App has come to the foreground!');
+        console.log('App has come to the foreground!')
         if (permissionsLocation.granted == true) {
-          getCurrentPosition();
+          getCurrentPosition()
         }
       }
 
-      appState.current = nextAppState;
-      setAppStateVisible(appState.current);
-      console.log('AppState', appState.current);
-    });
+      appState.current = nextAppState
+      setAppStateVisible(appState.current)
+      console.log('AppState', appState.current)
+    })
 
     return () => {
-      subscription.remove();
-    };
-  }, []);
+      subscription.remove()
+    }
+  }, [])
 
-  // After User has logged in
-  // Set the external user id in OneSignal 
-  // Set the user location in WP
-  if (carcalSession) {
-    (async () => {
-      const id = await getExternalUIDInWP(carcalSession);
+  useEffect(() => {
+    if (carcalSession) {
+      (async () => {
+        const id = await getExternalUIDInWP(carcalSession)
+        if (id === null) {
+          console.log(`Error getting external user id`)
+          return
+        }
 
-      if (location && location !== null) {
-        const res = await maybeSetUserLocation(location, id);
-        console.log(`Location status : ${res}`);
-      }
+        // Check if user is logged in
+        const res = await loginUserinWP(id)
+        if (res.success) {
+          // reload the webview
+          webViewRef.current.reload()
+        }
 
-      // Set the external user id in OneSignal
-      if (id && !onesignalRegistered) {
-        OneSignal.setExternalUserId(id);
-        setOnesignalRegistered(true);
-      }
-    })();
-  }
+        if (location && location !== null) {
+          const res = await maybeSetUserLocation(location, id)
+          // console.log(`Location status :`, res)
+        }
 
-  const INJECTED_JAVASCRIPT = `(function() {
-    const allData = window.localStorage.getItem('ccevents_ukey');
-    window.ReactNativeWebView.postMessage(allData);
-  })();`;
+        // Set the external user id in OneSignal
+        if (id && !onesignalRegistered) {
+          OneSignal.setExternalUserId(id)
+          setOnesignalRegistered(true)
+        }
+      })()
+    }
+  }, [carcalSession])
 
   const onMessage = (payload) => {
     if (payload.nativeEvent.data) {
-      setcarcalSession(payload.nativeEvent.data);
+      setcarcalSession(payload.nativeEvent.data)
     }
-  };
+  }
 
   return (
     <SafeAreaView style={{
@@ -186,12 +195,12 @@ export default function App() {
         }>
         <WebView
           ref={webViewRef}
-          source={{ uri: `https://staging1.carcalendar.co.uk/app/app.php?pid=${playerId}` }}
+          source={{ uri: `${URL}/app/app.php?pid=${playerId}` }}
           injectedJavaScript={INJECTED_JAVASCRIPT}
           onMessage={onMessage}
           onScroll={handleScroll}
         />
       </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
--- utils.js
+++ utils.js
@@ -1,13 +1,15 @@
-import { PermissionsAndroid } from 'react-native';
-import { Permissions } from 'expo-permissions';
+import { PermissionsAndroid } from 'react-native'
+import { Permissions } from 'expo-permissions'
+
+export const URL = 'https://staging1.carcalendar.co.uk'
 
 export async function setExpoTokenInWP(token) {
     // send request to server to save token
-    let url = 'https://www.carcalendar.co.uk/wp-json/expoapi/v1/expo_update_push_token';
+    let url = 'https://www.carcalendar.co.uk/wp-json/expoapi/v1/expo_update_push_token'
     let data = {
         deviceId: 22,
         token: token,
-    };
+    }
 
     let response = await fetch(url, {
         method: 'POST',
@@ -17,41 +19,69 @@ export async function setExpoTokenInWP(token) {
         body: JSON.stringify(data),
     })
 
-    let json = await response.json();
-    console.log(`Response from server: ${json}`);
+    let json = await response.json()
+    console.log(`Response from server: ${json}`)
 }
 
 export async function getExternalUIDInWP(carcalSession) {
     // send request to server to save token
-    let url = 'https://staging1.carcalendar.co.uk/wp-json/expoapi/v1/get_external_uid';
-    let data = {
-        token: carcalSession,
-    };
+    try {
+        let url = `${URL}/wp-json/expoapi/v1/get_external_uid`
+        let data = {
+            token: carcalSession,
+        }
 
-    let response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
 
-    let json = await response.json();
-    // format the external user id
-    // remove ""
-    id = json.replace(/"/g, '');
-    // remove spaces
-    id = id.replace(/\s/g, '');
-    return id;
+        let json = await response.json()
+        // format the external user id
+        // remove ""
+        id = json.replace(/"/g, '')
+        // remove spaces
+        id = id.replace(/\s/g, '')
+        return id
+    } catch (err) {
+        console.log(err)
+        return null
+    }
+}
+
+export async function loginUserinWP(user_id) {
+    // send request to server to save token
+    try {
+        let url = `${URL}/wp-json/expoapi/v1/login_user`
+        let data = {
+            user_id: user_id,
+        }
+
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        let json = await response.json()
+        return JSON.parse(json)
+    } catch (err) {
+        console.log(err)
+        return null
+    }
 }
 
 export async function maybeSetUserLocation(coords, uid) {
     // send request to server to save token
-    let url = 'https://staging1.carcalendar.co.uk/wp-json/expoapi/v1/set_user_location';
+    let url = `${URL}/wp-json/expoapi/v1/set_user_location`
     let data = {
         coords: coords,
         user_id: uid
-    };
+    }
 
     let response = await fetch(url, {
         method: 'POST',
@@ -61,8 +91,8 @@ export async function maybeSetUserLocation(coords, uid) {
         body: JSON.stringify(data),
     })
 
-    let json = await response.json();
-    return json;
+    let json = await response.json()
+    return json
 }
 
 export async function GetAllPermissions() {
@@ -72,11 +102,11 @@ export async function GetAllPermissions() {
                 PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
                 PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
                 PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
-            ]);
-            return userResponse;
+            ])
+            return userResponse
         }
     } catch (err) {
-        Warning(err);
+        Warning(err)
     }
-    return null;
+    return null
 }

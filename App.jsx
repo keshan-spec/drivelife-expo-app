d03c2d0dffca4dce01bc486dbe75f91b458d36c7
Keshanth Jude <keshanth64@gmail.com>
2024-06-27 15:53:50 +0100

    Updated refresh control/ Cleaned code

--- App.jsx
+++ App.jsx
@@ -1,14 +1,16 @@
-import { SafeAreaView, AppState, Alert, Linking, RefreshControl, ScrollView, BackHandler, Platform, ActivityIndicator, Image, View } from "react-native";
+import { SafeAreaView, AppState, Alert, Linking, ScrollView, BackHandler, Platform } from "react-native";
 import { useEffect, useState, useRef, useCallback } from 'react';
 import { WebView } from 'react-native-webview';
-import OneSignal from 'react-native-onesignal';
-import Constants from 'expo-constants';
+
 import 'expo-dev-client';
 import Geolocation from '@react-native-community/geolocation';
+import OneSignal from 'react-native-onesignal';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
 import BackgroundService from 'react-native-background-actions';
 
 // https://stackoverflow.com/questions/54075629/reactnative-permission-always-return-never-ask-again
-import { maybeSetUserLocation, getExternalUIDInWP, GetAllPermissions, loginUserinWP } from './utils';
+import { GetAllPermissions } from './utils';
 import CreatePost from "./CreatePost/CreatePostPage";
 import { addPost } from "./CreatePost/actions/create-post";
 
@@ -19,8 +21,6 @@ const INJECTED_JAVASCRIPT = `(function() {
     // window.ReactNativeWebView.postMessage(allData);
     window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'allData', data: allData }));
 })();`;
-
-import * as Notifications from 'expo-notifications';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -33,28 +33,23 @@ Notifications.setNotificationHandler({
 const URL = 'https://phpstack-889362-4370795.cloudwaysapps.com';
 
 export default function App() {
-  const [carcalSession, setcarcalSession] = useState('');
+  const [carcalSession, setCarcalSession] = useState('');
   const [onesignalRegistered, setOnesignalRegistered] = useState(false);
   const [playerId, setPlayerId] = useState('');
   const [location, setLocation] = useState();
   const [permissionsLocation, setPermissionsLocation] = useState({ denied: false, granted: false });
-  const [webviewLoaded, setWebviewLoaded] = useState(false);
 
   const [view, setView] = useState('webview');
 
   const appState = useRef(AppState.currentState);
-
-  const [refreshing, setRefreshing] = useState(false);
   const webViewRef = useRef();
 
-  const [refresherEnabled, setEnableRefresher] = useState(true);
   const [deepLinkUrl, setDeepLinkUrl] = useState('');
 
   // This state saves whether your WebView can go back
   const [webViewcanGoBack, setWebViewcanGoBack] = useState(false);
 
   useEffect(() => {
-
     const setNotifChannels = async () => {
       // create a channel with the custom notification sound
       await Notifications.setNotificationChannelAsync('primary', {
@@ -88,96 +83,6 @@ export default function App() {
     };
   }, []);
 
-  //Code to get scroll position
-  const handleScroll = (event) => {
-    const yOffset = Number(event.nativeEvent.contentOffset.y);
-    if (yOffset === 0) setEnableRefresher(true);
-    else setEnableRefresher(false);
-  };
-
-  const onRefresh = useCallback(() => {
-    setRefreshing(true);
-    // webViewRef.current.reload();
-    // inject js to refresh the page
-    webViewRef.current.injectJavaScript(`window.location.reload();`);
-  }, []);
-
-  // Get the users current location
-  const getCurrentPosition = () => {
-    Geolocation.getCurrentPosition((pos) => {
-      if (pos.coords) setLocation(pos.coords);
-    }, (error) => {
-      Alert.alert("CarCalendar", error.message, [
-        { text: "OK" },
-        {
-          text: "Settings",
-          onPress: () => {
-            Linking.openSettings();
-          }
-        }
-      ]);
-    },
-      { enableHighAccuracy: true }
-    );
-  };
-
-  const onMessage = async (payload) => {
-    const data = payload.nativeEvent.data;
-
-    try {
-      const message = JSON.parse(data);
-
-      if (message.type === 'auth-uid') {
-        setcarcalSession(payload.nativeEvent.data);
-      } else if (message.type === 'createPost') {
-        // check if background service is running
-        const isRunning = BackgroundService.isRunning();
-
-        if (isRunning) {
-          Alert.alert('CarCalendar', 'Please wait for the current post to finish uploading', [
-            { text: 'OK' }
-          ]);
-          return;
-        }
-
-        if (!message.user_id || message.user_id === '') {
-          await Notifications.scheduleNotificationAsync({
-            content: {
-              title: 'Authentication Required',
-              body: 'Please login to create a post',
-            },
-            trigger: null,
-            identifier: 'primary',
-          });
-          return;
-        }
-
-        setView('createPost');
-        setcarcalSession(message.user_id);
-        setWebviewLoaded(false);
-      }
-    } catch (error) {
-      console.error('Error parsing message:', error);
-    }
-  };
-
-  const handleBackPress = useCallback(() => {
-    try {
-      if (webViewRef.current) {
-        if (webViewcanGoBack) {
-          webViewRef.current.goBack(); // Attempt to go back within the WebView
-          return true; // Prevent default behavior (closing the app)
-        } else {
-          return false; // Default behavior (close the app)
-        }
-      }
-
-      return false;
-    } catch (error) {
-      console.log(`Error going back:`, error);
-      return false; // Default behavior (close the app)
-    }
-  }, [webViewcanGoBack]);
 
   // OneSignal Initialization
   useEffect(() => {
@@ -207,7 +112,10 @@ export default function App() {
 
         // Notifications
         if (res["android.permission.POST_NOTIFICATIONS"] === "granted") {
-          console.log('Notification permission granted');
+          OneSignal.getDeviceState().then((deviceState) => {
+            console.log(`OneSignal Player ID: ${deviceState.userId}`);
+            setPlayerId(deviceState.userId);
+          });
         } else {
           console.log('Notification permission denied');
         }
@@ -280,28 +188,20 @@ export default function App() {
   useEffect(() => {
     if (carcalSession) {
       (async () => {
-        const id = await getExternalUIDInWP(carcalSession);
-        if (id === null) {
-          console.log(`Error getting external user id`);
-          return;
-        }
 
-        // Check if user is logged in
-        const res = await loginUserinWP(id);
-
-        if (res.success) {
-          // reload the webview
-          webViewRef.current.reload();
-        }
-
-        if (location && location !== null) {
-          const res = await maybeSetUserLocation(location, id);
-          // console.log(`Location status :`, res)
-        }
+        // if (location && location !== null) {
+        //   const res = await maybeSetUserLocation(location, carcalSession);
+        //   // console.log(`Location status :`, res)
+        // }
 
         // Set the external user id in OneSignal
-        if (id && !onesignalRegistered) {
-          OneSignal.setExternalUserId(id);
+        if (carcalSession && !onesignalRegistered) {
+          OneSignal.setAppId(Constants.manifest.extra.onesignal.app_id);
+
+          console.log(`Setting external user id in OneSignal: ${carcalSession}, ${playerId}`);
+          OneSignal.setExternalUserId(`${carcalSession}`, playerId, (results) => {
+            console.log('OneSignal external user id set:', results);
+          });
           setOnesignalRegistered(true);
         }
       })();
@@ -343,6 +243,88 @@ export default function App() {
     }
   }, [carcalSession]);
 
+  // Get the users current location
+  const getCurrentPosition = () => {
+    Geolocation.getCurrentPosition((pos) => {
+      if (pos.coords) setLocation(pos.coords);
+    }, (error) => {
+      Alert.alert("CarCalendar", error.message, [
+        { text: "OK" },
+        {
+          text: "Settings",
+          onPress: () => {
+            Linking.openSettings();
+          }
+        }
+      ]);
+    },
+      { enableHighAccuracy: true }
+    );
+  };
+
+  const onMessage = async (payload) => {
+    const data = payload.nativeEvent.data;
+
+    try {
+      const message = JSON.parse(data);
+
+      if (message.type === 'authData') {
+        setCarcalSession(message.user_id);
+      } else if (message.type === 'createPost') {
+        const isRunning = BackgroundService.isRunning();
+
+        if (isRunning) {
+          Alert.alert('CarCalendar', 'Please wait for the current post to finish uploading', [
+            { text: 'OK' }
+          ]);
+          return;
+        }
+
+        if (!message.user_id || message.user_id === '') {
+          await Notifications.scheduleNotificationAsync({
+            content: {
+              title: 'Authentication Required',
+              body: 'Please login to create a post',
+            },
+            trigger: null,
+            identifier: 'primary',
+          });
+          return;
+        }
+
+        setView('createPost');
+        setCarcalSession(message.user_id);
+      }
+    } catch (error) {
+      console.error('Error parsing message:', error);
+    }
+  };
+
+  const handleBackPress = useCallback(() => {
+    try {
+      if (webViewRef.current) {
+        if (webViewcanGoBack) {
+          webViewRef.current.goBack(); // Attempt to go back within the WebView
+          return true; // Prevent default behavior (closing the app)
+        } else {
+          return false; // Default behavior (close the app)
+        }
+      }
+
+      return false;
+    } catch (error) {
+      console.log(`Error going back:`, error);
+      return false; // Default behavior (close the app)
+    }
+  }, [webViewcanGoBack]);
+
+  const userRegisteredInOneSignal = async () => {
+    if (carcalSession !== '' && carcalSession !== null) {
+      const deviceState = await OneSignal.getDeviceState();
+      return deviceState.userId === carcalSession;
+    }
+  };
+
   return (
     <SafeAreaView style={{
       flex: 1,
@@ -361,46 +343,16 @@ export default function App() {
             flex: 1,
             backgroundColor: '#fff',
           }}
-        // refreshControl={
-        //   <RefreshControl
-        //     refreshing={refreshing}
-        //     onRefresh={onRefresh}
-        //     enabled={refresherEnabled}
-        //   />
-        // }
         >
           <WebView
             ref={webViewRef}
-            // onLoadEnd={() => {
-            //   setWebviewLoaded(true);
-            //   setRefreshing(false);
-            // }}
-            source={{ uri: `https://phpstack-889362-4370795.cloudwaysapps.com${deepLinkUrl ? '?deeplink=' + deepLinkUrl : ''}` }}
-            // injectedJavaScript={INJECTED_JAVASCRIPT}
+            source={{ uri: `${URL}${deepLinkUrl ? '?deeplink=' + deepLinkUrl : ''}` }}
             onMessage={onMessage}
-            // onScroll={handleScroll}
             onLoadProgress={({ nativeEvent }) => {
               // This function is called everytime your web view loads a page
               // and here we change the state of can go back
               setWebViewcanGoBack(nativeEvent.canGoBack);
             }}
-          // style={{
-          //   flex: !refreshing ? 1 : 0,
-          // }}
-          // renderLoading={() => (
-          //   <View
-          //     style={{
-          //       flex: 1,
-          //       justifyContent: 'center',
-          //       alignItems: 'center',
-          //       height: '100%',
-          //       width: '100%',
-          //       backgroundColor: '#fff',
-          //     }}
-          //   >
-          //     <Image source={require('./assets/loading.gif')} style={{ width: 100, height: 100 }} />
-          //   </View>
-          // )}
           />
         </ScrollView>
       )}
